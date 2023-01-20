92fd98a317cd03d450cdfa53e2c4f7b50a17bbd4
yonnie-park <juon0805@gmail.com>
2023-01-20 15:16:15 +0900

    add: header, landing page content

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,52 @@
+import Image from 'next/image';
+import Header from '../src/components/Header'
 export default function Home() {
   return (
     <>
+    <Header/>
       <div className="landing">
-        <h1 className="landing__h1">랜딩페이지 입니다.</h1>
-        <p>this is landing</p>
+        <div className="landing__scroll1">
+          <h1 className="landing__h1__right">A New Way to Capture My Favorite Restaurants</h1> 
+          <Image
+            className="landing__illustration1"
+            src="/../public/images/Landing1.png"
+            alt="landing illustration"
+            width={400}
+            height={400}
+          />
+        </div>
+        <div className="landing__scroll2">
+          <Image
+            className="landing__illustration3"
+            src="/../public/images/Landing3.png"
+            alt="landing illustration"
+            width={400}
+            height={400}
+          />
+          <Image
+            className="landing__illustration2"
+            src="/../public/images/Landing2.png"
+            alt="landing illustration"
+            width={400}
+            height={400}
+          />
+          <h1 className="landing__h1-center">Scan and Receive Super Cool NFTs</h1>
+        </div>
+        <div className="landing__scroll3">
+          <h1 className="landing__h1-center">It is THIS easy</h1>
+          <div className="landing__scroll3-1">
+            <h2 className="landing__h2">1. Open Camera</h2>
+            <h2 className="landing__h2">2. Scan QR code</h2>
+            <h2 className="landing__h2">3. Get NFTs!</h2>
+          </div>
+        </div>
+        <div className="landing__scroll4">
+          <h1 className="landing__h1-center">Collect NFTs like these</h1>
+        </div>
+        <div className="landing__scroll5">
+          <button className="landing__button">Get Started</button>
+          <a className="landing__showOwner">Are You a Shop Owner?</a>
+        </div>
       </div>
     </>
   );
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div className="header">
+        <div className="header__logo">
+            <h1 className="header__h1">DiD You Eat?</h1>
+        </div>
+        <div className="header__button">
+            <button className="header__connect">Klip Connect</button>
+        </div>
+    </div>
+  )
+}
