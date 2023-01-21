fcdd7c3979b6055ccf0453b95d1f03a875569dab
oreozzang <93peace@naver.com>
2023-01-22 00:24:25 +0900

    add admin login,store,store-detail,admin-header,admin-footer

---
+++ client/pages/admin/store/index.tsx
@@ -0,0 +1,43 @@
+import AdminCollection from "@/src/components/AdminCollection";
+import AdminFooter from "@/src/components/AdminFooter";
+import AdminHeader from "@/src/components/AdminHeader";
+import Link from "next/link";
+
+export default function Store() {
+  return (
+    <div className="admin-store">
+      <AdminHeader></AdminHeader>
+      <h2 className="admin-store__title">나의 매장 목록보기</h2>
+      <div>
+        <div className="admin-store__collection-layout">
+          <Link href="/admin/store/1">
+            <AdminCollection></AdminCollection>
+          </Link>
+          <Link href="/admin/store/1">
+            <AdminCollection></AdminCollection>
+          </Link>
+          <Link href="/admin/store/1">
+            <AdminCollection></AdminCollection>
+          </Link>
+          <Link href="/admin/store/1">
+            <AdminCollection></AdminCollection>
+          </Link>
+          <Link href="/admin/store/1">
+            <AdminCollection></AdminCollection>
+          </Link>
+          <Link href="/admin/store/1">
+            <AdminCollection></AdminCollection>
+          </Link>
+          <Link href="/admin/store/1">
+            <AdminCollection></AdminCollection>
+          </Link>
+
+          <div className="admin-store__create-collection">
+            <span>새로운 매장 컬렉션을 생성합니다.</span>
+          </div>
+        </div>
+      </div>
+      <AdminFooter></AdminFooter>
+    </div>
+  );
+}
---
+++ client/src/components/AdminHeader.tsx
@@ -0,0 +1,13 @@
+export default function AdminHeader() {
+  return (
+    <nav className="admin-header">
+      <div className="admin-header__container">
+        <div className="admin-header__logo">Did you Eat?</div>
+        <div className="admin-header__li">
+          <div className="admin-header__item">login</div>
+          <div className="admin-header__item">mypage</div>
+        </div>
+      </div>
+    </nav>
+  );
+}
