65ccaaa486b4d41248d39068b7a3b666f6d9b2db
oreozzang <93peace@naver.com>
2023-01-27 19:27:22 +0900

    add: recoil user-id state

--- client/src/components/AdminHeader.tsx
+++ client/src/components/AdminHeader.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { useRecoilState } from "recoil";
+import { UserId } from "../recoil/states";
 
 export default function AdminHeader() {
+  const [userId, setUserId] = useRecoilState(UserId);
+
   return (
     <nav className="admin-header">
       <div className="admin-header__container">
@@ -8,8 +12,25 @@ export default function AdminHeader() {
           <div className="admin-header__logo">Did you Eat?</div>
         </Link>
         <div className="admin-header__li">
-          <div className="admin-header__item">Log in</div>
+          {userId === "" && (
+            <div
+              onClick={() => {
+                setUserId("jinwoo");
+              }}
+              className="admin-header__item">
+              Log in
+            </div>
+          )}
           <div className="admin-header__item">Sign up</div>
+          {userId !== "" && (
+            <div
+              onClick={() => {
+                setUserId("");
+              }}
+              className="admin-header__item">
+              {userId} Log out
+            </div>
+          )}
         </div>
       </div>
     </nav>
