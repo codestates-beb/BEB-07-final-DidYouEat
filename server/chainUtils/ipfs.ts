dd3c2f97d3b4650d05a73ec13b8442f776f52d41
DongMinKwon <wt0630@naver.com>
2023-01-28 18:22:58 +0900

    ipfs ready

--- server/chainUtils/ipfs.ts
+++ server/chainUtils/ipfs.ts
@@ -6,55 +6,19 @@ import ipfsClient from 'ipfs-http-client';
 const ROOT_DIR = path.join(__dirname, '..');
 dotenv.config({ path: `${ROOT_DIR}/.env` });
 
-// const { KAS_ACCESS_KEY, KAS_SECRET_KEY } = process.env;
 const { IPFS_PROJECT_ID, IPFS_SECRET_KEY } = process.env;
 
-// console.log(KAS_ACCESS_KEY, KAS_SECRET_KEY);
+// const testMeta = {
+//   collection_id: 'test',
+//   img_url: 'https://test.url',
+//   coordinate_x: 36,
+//   coordinate_y: 48,
+//   owner_id: 'owner2',
+//   shop_name: 'fire',
+//   event: 'good event',
+// };
 
-const testMeta = {
-  collection_id: 'test',
-  img_url: 'https://test.url',
-  coordinate_x: 36,
-  coordinate_y: 48,
-  owner_id: 'owner1',
-  shop_name: 'fire',
-  event: 'good event',
-};
-
-// async function upload(meta) {
-//   //infura auth option
-//   const option = {
-//     headers: [
-//       {
-//         name: 'Authorization',
-//         value: `Basic ${Buffer.from(
-//           `${KAS_ACCESS_KEY}:${KAS_SECRET_KEY}`,
-//         ).toString('base64')}`,
-//       },
-//       { name: 'x-chain-id', value: '8217' },
-//     ],
-//   };
-
-//   console.log(option);
-
-//   //set api provider
-//   const caver = new Caver(
-//     new Caver.providers.HttpProvider(
-//       'https://node-api.klaytnapi.com/v1/klaytn',
-//       option,
-//     ),
-//   );
-
-//   //set infura endpoint
-//   await caver.ipfs.setIPFSNode('ipfs.infura.io', 5001, true);
-
-//   const cid = await caver.ipfs.add(Buffer.from(JSON.stringify(meta)));
-
-//   //debug cid
-//   console.log(`cid: ${cid}`);
-// }
-
-async function upload(meta) {
+async function uploadIpfs(meta) {
   const auth = `Basic ${Buffer.from(
     `${IPFS_PROJECT_ID}:${IPFS_SECRET_KEY}`,
   ).toString('base64')}`;
@@ -68,9 +32,8 @@ async function upload(meta) {
     },
   });
 
-  client.add(JSON.stringify(meta)).then((res) => {
-    console.log(res);
-  });
+  const cid = await (await client.add(JSON.stringify(meta))).path;
+  return cid;
 }
 
-upload(testMeta);
+export { uploadIpfs };
