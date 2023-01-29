7bcf364649af3c7b4570cc56a8e523e6428c4f00
yonnie-park <juon0805@gmail.com>
2023-01-29 11:27:31 +0900

    add: Klip connection

--- client/src/components/KlipButton.tsx
+++ client/src/components/KlipButton.tsx
@@ -1,20 +1,18 @@
-//import { prepare, request, getResult } from "klip-sdk";
+import { prepare, request, getResult } from "klip-sdk";
 
 export default function KlipButton() {
   async function handleConnect() {
-    // const bappName = "DiD You Eat?";
-    // const successLink = "/";
-    // //const failLink = "/failed";
-    // const res = await prepare.auth({ bappName, successLink });
-    // if (res.err) {
-    //   console.log(res.err);
-    // }
-    // const requestKey = res.request_key;
+    const bappName = "DiD You Eat?";
+    const successLink = "/";
+    const failLink = "/failed";
+    const res = await prepare.auth({ bappName, successLink, failLink });
+    if (res.err) {
+      console.log(res.err);
+    }
+    const requestKey = res.request_key;
 
-    // request(requestKey, () => alert("모바일 환경에서 실행해주세요"));
-    // console.log(getResult(requestKey));
-    if (typeof window === undefined) console.log("window undefined");
-    else console.log(window.location);
+    request(requestKey, () => alert("모바일 환경에서 실행해주세요"));
+    console.log(getResult(requestKey));
   }
 
   return (
