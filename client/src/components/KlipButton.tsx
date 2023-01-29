32f3621898f636655c2cf94a6c2af55ca34f6533
yonnie-park <juon0805@gmail.com>
2023-01-29 11:37:54 +0900

    edit: klip failed,,

--- client/src/components/KlipButton.tsx
+++ client/src/components/KlipButton.tsx
@@ -1,8 +1,8 @@
-import { prepare, request, getResult } from "klip-sdk";
+//import { prepare, request, getResult } from "klip-sdk";
 
 export default function KlipButton() {
   async function handleConnect() {
-    const bappName = "DiD You Eat?";
+    /* const bappName = "DiD You Eat?";
     const successLink = "/";
     const failLink = "/failed";
     const res = await prepare.auth({ bappName, successLink, failLink });
@@ -12,7 +12,8 @@ export default function KlipButton() {
     const requestKey = res.request_key;
 
     request(requestKey, () => alert("모바일 환경에서 실행해주세요"));
-    console.log(getResult(requestKey));
+    console.log(getResult(requestKey)); */
+    console.log("왜안돼");
   }
 
   return (
