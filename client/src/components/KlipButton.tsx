2c9f14be978ffc32c4553c92ab174ef132aa62b6
natreeum <projectkmscom@gmail.com>
2023-01-29 14:58:35 +0900

    add : klip auth

--- client/src/components/KlipButton.tsx
+++ client/src/components/KlipButton.tsx
@@ -1,19 +1,44 @@
-//import { prepare, request, getResult } from "klip-sdk";
+import axios from 'axios'
 
 export default function KlipButton() {
   async function handleConnect() {
-    /* const bappName = "DiD You Eat?";
-    const successLink = "/";
-    const failLink = "/failed";
-    const res = await prepare.auth({ bappName, successLink, failLink });
-    if (res.err) {
-      console.log(res.err);
+    const userAgent = window.navigator.userAgent
+    const platform = window.navigator.platform
+    const macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K']
+    const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE']
+    const iosPlatforms = ['iPhone', 'iPad', 'iPod']
+    let os = null
+    if (macosPlatforms.indexOf(platform) !== -1) {
+      os = 'MacOS';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+      os = 'iOS';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+      os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+      os = 'Android';
+    } else if (!os && /Linux/.test(platform)) {
+      os = 'Linux';
     }
-    const requestKey = res.request_key;
+    const isMobile = os === 'iOS' || os === 'Android' ? true : false
+    const getKlipPrepareUrl = ( request_key:string)=>{
+      if(isMobile){
+        return window.location.href = `kakaotalk://klipwallet/open?url=https://klipwallet.com/?target=/a2a?request_key=${request_key}`
+      }
+      else return console.log(`https://klipwallet.com/?target=/a2a?request_key=${request_key}`)
+    }
 
-    request(requestKey, () => alert("모바일 환경에서 실행해주세요"));
-    console.log(getResult(requestKey)); */
-    console.log("왜안돼");
+    const bappName = "DiD You Eat?";
+    const getAddress = await axios.post('https://a2a-api.klipwallet.com/v2/a2a/prepare',{bapp:{name:bappName},type : 'auth'})
+    const {request_key} = getAddress.data
+    getKlipPrepareUrl(request_key)
+    const timer = setInterval(()=>{
+      axios.get(`https://a2a-api.klipwallet.com/v2/a2a/result?request_key=${request_key}`).then((res)=>{
+        if(res.data.result){
+          alert(res.data.result.klaytn_address)
+          clearInterval(timer)
+        }
+      })
+    },1000)
   }
 
   return (
