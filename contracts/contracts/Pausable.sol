a1129f9b5c45511e22509e9de11f001870e5cc1f
DongMinKwon <wt0630@naver.com>
2023-01-29 11:51:46 +0900

    add comments

--- contracts/contracts/Pausable.sol
+++ contracts/contracts/Pausable.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "../node_modules/@klaytn/contracts/access/Ownable.sol";
 
 abstract contract Pausable is Ownable {
-
   //event
   event Paused(address account);
   event Unpaused(address account);
